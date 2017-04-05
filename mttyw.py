706dcca596e5652faa843a605783818834db6744
Simon A. F. Lund <safl@safl.dk>
2017-04-05 23:40:43 +0200

    ...

--- mttyw.py
+++ mttyw.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import inspect
 from flask import Flask, render_template, session, request
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
     close_room, rooms, disconnect
@@ -18,15 +19,17 @@ thread = None
 
 def background_thread():
     """Example of how to send server generated events to clients."""
+
     count = 0
     while True:
         socketio.sleep(10)
         count += 1
-        socketio.emit(
-	    'my_response',
-            {'data': 'Server generated event', 'count': count},
-            namespace='/test'
-	)
+
+        payload = {
+            'data': 'Server generated event',
+            'count': count
+        }
+        socketio.emit('my_response', payload, namespace='/mtty')
 
 @app.route('/')
 def index():
@@ -56,71 +59,123 @@ def logs(path):
 
     return send_from_directory(logs_root, os.path.basename(path))
 
-@socketio.on('my_event', namespace='/test')
-def test_message(message):
+@socketio.on('my_event', namespace='/mtty')
+def mtty_message(message):
+    print(inspect.currentframe().f_code.co_name)
+    
     session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']})
+    
+    payload = {
+        'data': message['data'],
+        'count': session['receive_count']
+    }
+    emit('my_response', payload)
 
-@socketio.on('my_broadcast_event', namespace='/test')
-def test_broadcast_message(message):
+@socketio.on('my_broadcast_event', namespace='/mtty')
+def mtty_broadcast_message(message):
+    print(inspect.currentframe().f_code.co_name)
+    
     session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         broadcast=True)
+   
+    payload = {
+        'data': message['data'],
+        'count': session['receive_count']
+    }
+    emit('my_response', payload, broadcast=True)
 
-@socketio.on('join', namespace='/test')
-def join(message):
-    join_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
-
-@socketio.on('leave', namespace='/test')
-def leave(message):
-    leave_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
-
-@socketio.on('close_room', namespace='/test')
-def close(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response', {'data': 'Room ' + message['room'] + ' is closing.',
-                         'count': session['receive_count']},
-         room=message['room'])
-    close_room(message['room'])
-
-@socketio.on('my_room_event', namespace='/test')
-def send_room_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         room=message['room'])
-
-@socketio.on('disconnect_request', namespace='/test')
-def disconnect_request():
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'Disconnected!', 'count': session['receive_count']})
-    disconnect()
-
-@socketio.on('my_ping', namespace='/test')
-def ping_pong():
-    emit('my_pong')
-
-@socketio.on('connect', namespace='/test')
-def test_connect():
+@socketio.on('connect', namespace='/mtty')
+def mtty_connect():
     global thread
+    print(inspect.currentframe().f_code.co_name)
+    
+
     if thread is None:
         thread = socketio.start_background_task(target=background_thread)
-    emit('my_response', {'data': 'Connected', 'count': 0})
+    payload = {
+        'data': 'Connected',
+        'count': 0
+    }
+    emit('my_response', payload)
 
-@socketio.on('disconnect', namespace='/test')
-def test_disconnect():
+@socketio.on('disconnect', namespace='/mtty')
+def mtty_disconnect():
+    print(inspect.currentframe().f_code.co_name)
+    
     print('Client disconnected', request.sid)
+
+@socketio.on('join', namespace='/mtty')
+def join(message):
+    print(inspect.currentframe().f_code.co_name)
+    
+    join_room(message['room'])
+    
+    session['receive_count'] = session.get('receive_count', 0) + 1
+   
+    payload = {
+        'data': 'In rooms: ' + ', '.join(rooms()),
+        'count': session['receive_count']
+    }
+    emit('my_response', payload)
+
+@socketio.on('leave', namespace='/mtty')
+def leave(message):
+    print(inspect.currentframe().f_code.co_name)
+    
+    leave_room(message['room'])
+    
+    session['receive_count'] = session.get('receive_count', 0) + 1
+   
+    payload = {
+        'data': 'In rooms: ' + ', '.join(rooms()),
+        'count': session['receive_count']
+    }
+    emit('my_response', payload)
+
+@socketio.on('close_room', namespace='/mtty')
+def close(message):
+    print(inspect.currentframe().f_code.co_name)
+    
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    
+    payload = {
+        'data': 'Room ' + message['room'] + ' is closing.',
+        'count': session['receive_count']
+    }
+    emit('my_response', payload, room=message['room'])
+    
+    close_room(message['room'])
+
+@socketio.on('my_room_event', namespace='/mtty')
+def send_room_message(message):
+    print(inspect.currentframe().f_code.co_name)
+    
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    
+    payload = {
+        'data': message['data'],
+        'count': session['receive_count']
+    }
+    emit('my_response', payload, room=message['room'])
+
+@socketio.on('disconnect_request', namespace='/mtty')
+def disconnect_request():
+    print(inspect.currentframe().f_code.co_name)
+    
+    session['receive_count'] = session.get('receive_count', 0) + 1
+
+    payload = {
+        'data': 'Disconnected!',
+        'count': session['receive_count']
+    }
+    emit('my_response', payload)
+    
+    disconnect()
+
+@socketio.on('my_ping', namespace='/mtty')
+def ping_pong():
+    print(inspect.currentframe().f_code.co_name)
+    
+    emit('my_pong')
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
